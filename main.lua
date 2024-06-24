13dd667e5405cc24a320f22c5e8ff34968a73127
ITemply <120138779+ITemply@users.noreply.github.com>
2024-06-24 00:01:57 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -14,33 +14,33 @@ local revolutions = 0
 local alwaysOn = true
 
 local getKills = function(damageEvent)
-  task.spawn(function()
-    while true do
-      for id, entity in ipairs(NPCS:GetChildren()) do
-        if entity:FindFirstChild('Humanoid') then
-          local s, e = pcall(function()
-            local hitBoss = entity:FindFirstChild('Humanoid')
-            
-            local local damageArgs = {
-              [1] = hitBoss,
-              [2] = 1
-            }
+	task.spawn(function()
+		while true do
+			for id, entity in ipairs(NPCS:GetChildren()) do
+				if entity:FindFirstChild('Humanoid') then
+					local s, e = pcall(function()
+						local hitBoss = entity:FindFirstChild('Humanoid')
 
-            repeat
-			        damageEvent:FireServer(unpack(damageArgs))
-			        task.wait()
-		        until hitBoss.Health <= 0 or client.Character.Humanoid.Health <= 0
-          end)
-        end
-      end
-      task.wait(1)
-    end
-  end)
+						local local damageArgs = {
+							[1] = hitBoss,
+							[2] = 1
+						}
+
+						repeat
+							damageEvent:FireServer(unpack(damageArgs))
+							task.wait()
+						until hitBoss.Health <= 0 or client.Character.Humanoid.Health <= 0
+					end)
+				end
+			end
+			task.wait(1)
+		end
+	end)
 end
 
 local keepAlive = function()
-  client.Idled:Connect(function()
-    virtualUser:CaptureController()
+	client.Idled:Connect(function()
+		virtualUser:CaptureController()
 		virtualUser:ClickButton2(Vector2.new())
 	end)
 end
